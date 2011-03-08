b2ff732b2425c479af43f962b7a51fdf52ed99da
Florian Aßmann <flazy@fork.de>
2011-03-08 12:31:49 +0100

    Need to provide custom hit and miss functions.

--- dullahan/public/~/js/application.js
+++ dullahan/public/~/js/application.js
@@ -161,7 +161,9 @@ jQuery(function($) {
 		});
 
 		utils.fuzzy(column.find('.type-select .pattern'), {
-			filter: '#' + column.attr('id') + ' tbody tr td.name'
+			filter: '#' + column.attr('id') + ' tbody tr td.name',
+			hit: function(td) { td.parent().css('display', 'table-row'); },
+			miss: function(td) { td.parent().css('display', 'none'); }
 		});
 
 		var anchors = column.find('th a');
